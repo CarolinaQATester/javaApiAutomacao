5af62c8e429339818cc625f0c8aecc814bf4e353
CarolinaQATester <chmqateste@gmail.com>
2021-12-02 16:30:58 -0300

    Atualização

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,7 +5,6 @@ public class AppTest {
     
     @Test 
     public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
     }
 }
