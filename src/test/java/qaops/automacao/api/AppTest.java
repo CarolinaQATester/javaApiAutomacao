90074e86b4b2773dd4fcd663cf4bc593cfd25e02
CarolinaQATester <chmqateste@gmail.com>
2021-12-03 14:49:50 -0300

    adicionando teste de criação de usuario

--- src/test/java/qaops/automacao/api/AppTest.java
+++ src/test/java/qaops/automacao/api/AppTest.java
@@ -1,13 +1,41 @@
 package qaops.automacao.api;
 
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.*;
+
 
 public class AppTest {
     
     @Test 
-    public void testAppHasAGreeting() {
+    public void testeListaMetadosDoUsuario(){
+       when().
+               get("https://reqres.in/api/users?page=2").
+       then().
+               log().all().
+               statusCode(HttpStatus.SC_OK).
+               body("page", is(2)).
+               body("data", is(notNullValue()));
+    }
+    @Test
+    public void testeCriaUsuairoComSucesso(){
+        given().log().all().
+                contentType(ContentType.JSON).
+                body("{" +
+                                "\"email\": \"carol@reque.in\"," +
+                                "\"first_name\": \"Carol\"," +
+                                "\"last_name\": \"Mesquita\",\"avatar\": \"https://reqres.in/img/faces/7-image.jpg\"}").
+        when().log().all().
+                post("https://reqres.in/api/users").
+        then().
+                log().all().
+                statusCode(HttpStatus.SC_CREATED).
+                body("first_name", is("Carol"));
+
     }
 }
+
