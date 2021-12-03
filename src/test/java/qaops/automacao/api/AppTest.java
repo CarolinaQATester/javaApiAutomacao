57c7f1b77ee7143b83b600d2b11d40127e5887c4
CarolinaQATester <chmqateste@gmail.com>
2021-12-03 14:54:11 -0300

    Habilitar log verboso para toda classe

--- src/test/java/qaops/automacao/api/AppTest.java
+++ src/test/java/qaops/automacao/api/AppTest.java
@@ -1,7 +1,9 @@
 package qaops.automacao.api;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,29 +12,33 @@ import static org.hamcrest.CoreMatchers.*;
 
 
 public class AppTest {
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
     
     @Test 
     public void testeListaMetadosDoUsuario(){
        when().
                get("https://reqres.in/api/users?page=2").
        then().
-               log().all().
+
                statusCode(HttpStatus.SC_OK).
                body("page", is(2)).
                body("data", is(notNullValue()));
     }
     @Test
     public void testeCriaUsuairoComSucesso(){
-        given().log().all().
+        given().
                 contentType(ContentType.JSON).
                 body("{" +
                                 "\"email\": \"carol@reque.in\"," +
                                 "\"first_name\": \"Carol\"," +
                                 "\"last_name\": \"Mesquita\",\"avatar\": \"https://reqres.in/img/faces/7-image.jpg\"}").
-        when().log().all().
+        when().
                 post("https://reqres.in/api/users").
         then().
-                log().all().
                 statusCode(HttpStatus.SC_CREATED).
                 body("first_name", is("Carol"));
 
